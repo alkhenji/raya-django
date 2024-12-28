6848fbf8357fdad0c42464b141a8dd0fa443f521
Abdulla AlKhenji <alkhenji@users.noreply.github.com>
2024-12-29 00:27:59 +0300

    Added deals, startups, investors pages

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,4 +5,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('deals/', views.DealsListView.as_view(), name='deals'),
+    path('startups/', views.StartupsListView.as_view(), name='startups'),
+    path('investors/', views.InvestorsListView.as_view(), name='investors'),
 ] 
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,13 @@
 from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
-    template_name = 'home.html' 
+    template_name = 'home.html'
+
+class DealsListView(TemplateView):
+    template_name = 'core/deals_list.html'
+
+class StartupsListView(TemplateView):
+    template_name = 'core/startups_list.html'
+
+class InvestorsListView(TemplateView):
+    template_name = 'core/investors_list.html' 
